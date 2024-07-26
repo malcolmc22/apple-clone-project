e09f673166d5a246be1ed3ff282911fdabe96e08
Malcolm Caleb <malcolmcaleb01@gmail.com>
2024-07-26 13:45:47 -0400

    finished nav css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 // rafce with the extension to quickly make the component
 import React from 'react'
 import { appleImg, bagImg, searchImg } from '../utils'
+// instead of the array we can import our navlist from our constants and map over that instead
+import { navLists } from '../constants'
 const Navbar = () => {
   return (
     // w-full so it takes the full width
@@ -11,18 +13,19 @@ const Navbar = () => {
         {/* this will use the screen-max-width in our index.css */}
         <nav className='screen-max-width flex w-full'>
             <img src={appleImg} width={14} height={18} />
-
-            <div>
+            {/* devices with a width of 600 or less applies to max-sm */}
+            <div className='flex flex-1 justify-center max-sm:hidden'>
                 {/* This will be all of our nav items made dynamically, we put the nav items in an array and map over them getting the nav item
                 and it's index, and render it with divs */}
-                {['Phones', 'Macbooks', 'Tablets'].map((nav, i) =>(
-                    <div key={i}>
+                {/* ['Phones', 'Macbooks', 'Tablets'] */ navLists.map((nav, i) =>(
+                    // transition to add a nice effect when the text goes from gray to white
+                    <div key={i} className='cursor-pointer px-5 text-sm text-gray transition-all hover:text-white'>
                         {nav}
                     </div>
                 ))}
             </div>
 
-            <div>
+            <div className='flex items-baseline gap-7 max-sm:flex-1 max-sm:justify-end'>
                 <img src={searchImg} alt='search' width={18} height={18}/>
                 <img src={bagImg} alt='bag' width={18} height={18}/>
             </div>
