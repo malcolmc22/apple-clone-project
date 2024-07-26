b6adfe8fee1de714c176b9082695d8536e044dbd
Malcolm Caleb <malcolmcaleb01@gmail.com>
2024-07-26 13:28:06 -0400

    added navbar items, search and bag icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,28 @@
 // rafce with the extension to quickly make the component
 import React from 'react'
-
+import { appleImg, bagImg, searchImg } from '../utils'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header>
+        <nav>
+            <img src={appleImg} width={14} height={18} />
+
+            <div>
+                {/* This will be all of our nav items made dynamically, we put the nav items in an array and map over them getting the nav item
+                and it's index, and render it with divs */}
+                {['Phones', 'Macbooks', 'Tablets'].map((nav, i) =>(
+                    <div key={i}>
+                        {nav}
+                    </div>
+                ))}
+            </div>
+
+            <div>
+                <img src={searchImg} alt='search' width={18} height={18}/>
+                <img src={bagImg} alt='bag' width={18} height={18}/>
+            </div>
+        </nav>
+    </header>
   )
 }
 
