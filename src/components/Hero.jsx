7f15f16ac57618180c58fca375e40164fdcc6238
Malcolm Caleb <malcolmcaleb01@gmail.com>
2024-08-03 17:34:53 -0400

    added apple phone video

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
+import { heroVideo, smallHeroVideo } from '../utils'
 // npm install gsap @gsap/react to get gsap packages
 const Hero = () => {
+  const [videoSrc, setVideoSrc] = useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo)
+
+  c
   useGSAP(() => {
     gsap.to('#hero', { opacity : 1, delay: 1.5})
   },[])
@@ -13,6 +17,11 @@ const Hero = () => {
       <div className='flex-center h-5/6 w-full flex-col'>
       {/* hero title dissapears after adding the classname becauser in our index we have hero-tile with set attributes */}
       <p id = 'hero' className='hero-title'>iPhone 15 Pro</p>
+      <div className='w-9/12 md:w-10/12'>
+      <video className='pointer-events-none' src={videoSrc} type='video/mp4' autoPlay muted playsInline={true} key={videoSrc}>
+
+        </video>
+      </div>
       </div>
     </section>
   )
